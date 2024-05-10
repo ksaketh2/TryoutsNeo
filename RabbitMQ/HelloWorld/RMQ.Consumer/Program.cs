b17646f1b50c5bfd25b8836735676f0957b9b700
Saketh Konakanchi <ksaketh2@hotmail.com>
2024-05-10 19:24:00 +0530

    --added rabbitmq hw projects

---
+++ RabbitMQ/HelloWorld/RMQ.Consumer/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RMQ.Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                byte[] body = ea.Body.ToArray();
+                string message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received {message}");
+            };
+            channel.BasicConsume(queue: "hello",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
